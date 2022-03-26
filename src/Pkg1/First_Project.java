9b5c95ce29f1aa86bb847491b0a08bec46ad7ba6
AkshatSinghal123 <88577880+AkshatSinghal123@users.noreply.github.com>
2022-03-26 12:46:50 +0530

    "Second commit"

--- src/Pkg1/First_Project.java
+++ src/Pkg1/First_Project.java
@@ -5,6 +5,7 @@ public class First_Project {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello World");
+        System.out.println("This is my Project");
 	}
 
 }
